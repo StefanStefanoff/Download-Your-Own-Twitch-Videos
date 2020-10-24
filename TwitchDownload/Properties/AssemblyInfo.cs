ee1ac3623aec8b21b67c2bcce93b0a227c631d25
Stefan Stefanov <stefan.stefanov@dnhsoft.com>
2020-10-24 06:27:21 +0300

    Change AssemblyInfo

--- TwitchDownload/Properties/AssemblyInfo.cs
+++ TwitchDownload/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TwitchGownload")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TwitchDownload")]
+[assembly: AssemblyDescription("Download your videos/highlights for archiving reasons")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TwitchGownload")]
+[assembly: AssemblyCompany("No Company")]
+[assembly: AssemblyProduct("TwitchDownload")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
